d112b3e401e8ca763606860af22ca555e571eb94
endlessid <endlessid@gmail.com>
2014-08-29 17:21:38 +0800

    bagui829

---
+++ Assets/Scripts/Bag/BagController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BagController : MonoBehaviour {
+
+	public UIItemStorage cardMatrix;
+	public CardCube[] cubeArray;
+
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		cubeArray = cardMatrix.GetComponentsInChildren<CardCube>();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SpiltPage(int pageIndex){
+		int pageMaxSlot = cubeArray.Length;
+		
+
+	}
+}
---
+++ Assets/Scripts/Bag/CardCube.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardCube : MonoBehaviour {
+	public int cubeID;
+	public UILabel showID;
+
+
+	// Use this for initialization
+	void Start () {
+		SetCubeData();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void SetCubeData(){
+		cubeID = Random.Range (1,100);
+		showID.text = cubeID.ToString();
+
+	}
+
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     

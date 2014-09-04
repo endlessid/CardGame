85570e2fa71bda22ee99c81c76dd959113cd4ab3
endlessid <endlessid@gmail.com>
2014-09-04 17:46:19 +0800

    942

--- Assets/Scripts/Controller/BattleController.cs
+++ Assets/Scripts/Controller/BattleController.cs
@@ -15,7 +15,7 @@ public class BattleController : MonoBehaviour {
 	public Player myPlayer;
 	public StageController sc;
 	public TextAsset cardAsset;
-	List<object> cards;
+	List<object> cardsList;
 //	public bool debug;
 //	public bool debug1;
 	// Use this for initialization
@@ -23,7 +23,7 @@ public class BattleController : MonoBehaviour {
 	void Start () {
 		//load card info from txt and make them a list<>
 		Dictionary<string,object>  cardDic  = MiniJSON.Json.Deserialize(cardAsset.text) as Dictionary<string,object>;
-		cards = cardDic["cards"]as List<object>;
+		cardsList = cardDic["cards"]as List<object>;
 
 	}
 	
@@ -39,7 +39,7 @@ public class BattleController : MonoBehaviour {
 	public void GenerateCard(int count){
 		for(int i = 0;i < count ;i++){
 			GameObject newCard = NGUITools.AddChild(cardDeck.gameObject,cardPrefab);
-			Dictionary<string,object> cardInfo = cards[i] as Dictionary<string,object>;
+			Dictionary<string,object> cardInfo = cardsList[i] as Dictionary<string,object>;
 			newCard.GetComponent<AbstractCard>().cHeart= int.Parse(cardInfo["heart"].ToString());
 			newCard.GetComponent<AbstractCard>().cDescription=cardInfo["description"].ToString();
 			newCard.GetComponent<AbstractCard>().cArmor=int.Parse(cardInfo["armor"].ToString());
@@ -48,11 +48,9 @@ public class BattleController : MonoBehaviour {
 			newCard.transform.RotateAround(newCard.transform.position,Vector3.up,180);
 			newCard.GetComponent<AbstractCard>().myBar = abBar ;
 			newCard.GetComponent<AbstractCard>().UpdateCardLabel();
-
-
 		}
 		if(count>0){
-			cards.RemoveRange(0,count);
+			cardsList.RemoveRange(0,count);
 
 		}
 		cardDeck.Reposition();
@@ -72,7 +70,8 @@ public class BattleController : MonoBehaviour {
 		StartCoroutine("CreatureCardAttack");
 	}
 
-	IEnumerator CreatureCardAttack(){	
+	IEnumerator CreatureCardAttack(){
+		//selected cards excute attack move
 		AbstractCard[] cards = cardDeck.GetComponentsInChildren<AbstractCard>();
 		for(int i = cards.Length-1;i>=0;i--){
 			if(cards[i].IsSelect){
@@ -81,8 +80,15 @@ public class BattleController : MonoBehaviour {
 				yield return new WaitForSeconds(0.6f);						
 			}
 		}
+		//fill vacancies with cards &check vacancies and cardlist which one is bigger in case of index out range
 		AbstractCard[] remaincards = cardDeck.GetComponentsInChildren<AbstractCard>();
-		GenerateCard(5 - remaincards.Length);
+		if(cardsList.Count < 5-remaincards.Length){
+		GenerateCard(cardsList.Count);
+		}
+		else{
+			GenerateCard(5-remaincards.Length);
+		}
+		//deal damage to enemy if its reach 0 change panel &update bars
 		if(myEnemy.EnemyHP >0){		
 			abBar.HPbar -=myEnemy.EnemyDMG;
 			yield return new WaitForSeconds(1f);
@@ -96,12 +102,12 @@ public class BattleController : MonoBehaviour {
 		}
 		abBar.MPbar = 100;
 		abBar.BarUpdate();
+		//make cards back to unselected
 		AbstractCard[] allcards = cardDeck.GetComponentsInChildren<AbstractCard>();
 		for(int i = allcards.Length -1 ;i>=0;i--){
 			cards[i].IsSelect = false;
 			}
 		}
-
 
 	}
 
